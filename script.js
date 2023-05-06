7b39e4faa2e739f262057fac30ff786922d0b72d
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2023-05-06 12:22:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -347,6 +347,23 @@ var c3 = new Car(5,20);
 
 
 
+// closures
+//closures
+var i =10;
+function outer(){
+    var j =20;
+    var inner = function(){
+        var k =30;
+        console.log(i,j,k);
+        i++;
+        j++;
+        k++;
+    }
+    return inner;
+}
 
+var inner = outer();
+inner();
+inner();
 
     
